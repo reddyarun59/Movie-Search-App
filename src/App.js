a828edb1b12741a0ad42bc66e346c1c4d62a0cf2
reddyarun59 <reddyarun59@yahoo.com>
2022-05-05 05:16:44 +0530

    Changed theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import SearchMovies from './SearchMovies'
 const App = () => {
   return ( 
-          <div className="box-border container mx-auto mt-24 px-8">
+          <div className="box-border container mx-auto mt-24">
             <h1 className="flex justify-center text-4xl font-extrabold mb-8">Movie Search App</h1>
             <SearchMovies className="mx-auto"/>
           </div> );
