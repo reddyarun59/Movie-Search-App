435cffd9ea572262952923a7e2d27ba7628c3053
reddyarun59 <reddyarun59@yahoo.com>
2022-05-05 05:11:43 +0530

    Changed theme

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,6 +1,6 @@
 const MovieCard = (props) => {
     return ( 
-        <div className="flex flex-col justify-items-center shadow-2xl shadow-gray-900 my-16 rounded-xl mx-24">
+        <div className="flex flex-col justify-items-center shadow-2xl shadow-gray-900 my-16 rounded-xl">
                 <img className="w-3/12 flex mx-auto my-8 rounded-lg" src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${props.movie.poster_path}`}alt={props.movie.title + ' poster'}/>
                 <div>
                     <h2 className="text-2xl font-bold px-8 my-4">{props.movie.title}</h2>
